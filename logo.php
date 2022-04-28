01398d5183d31cd3b9dd5e5fa7d8f40fde71dca8
Kenneth Brook <kenneth.brook@hotmail.com>
2022-04-28 14:38:52 -0400

    working

--- logo.php
+++ logo.php
@@ -3,13 +3,14 @@
 
 <head>
     <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/compressed.js"></script>
     <link href="logo.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="logoWrap">
         <img src="assets/img/logoani1.png" />
-        <h2 class="typeText">OF COASTAL<span class="startTyper"></span></h2>
+        <h2 class="typeText">OF COASTAL <span class="startTyper"></span></h2>
     </div>
 
 
@@ -18,9 +19,9 @@
     <script>
         $(function() {
         $(".startTyper").typed({
-        strings: ["Hi, I'm Hitesh S Vikani, a software developer from Mumbai","This is CSS typing effect", "I think it's cool,isn't it?", "You can download files from github.com", "Have a good one!"],
-        typeSpeed: 50, // typing speed
-        backDelay: 100, // pause before backspacing
+        strings: ["GEORGIA","JACKSONVILLE"],
+        typeSpeed: 250, // typing speed
+        backDelay: 200, // pause before backspacing
         loop: true, // loop on or off (true or false)
         loopCount: false, // number of loops, false = infinite
         });
